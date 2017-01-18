a6a7d03115bcf9c8061b5ad55ea8978dd3d7bded
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-01-18 11:07:57 -0500

    fin3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,21 +14,21 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path
+      redirect_to products_path
     end
   end
 
    def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to root_path
+      redirect_to products_path
     end
   end
 
   def destroy
     @product = Product.find(params[:id])
     if @product.destroy
-      redirect_to root_path
+      redirect_to products_path
     end
 
   end
